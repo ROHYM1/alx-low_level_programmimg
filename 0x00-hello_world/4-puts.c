f638d5715c97cbc2854f347acec22c64c45ffe0e
Gbenga <slamyg4u@ymail.com>
2022-09-12 08:29:37 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - entry point
+ *
+ * return: 0 after printing the function
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multiligual puzzles");
+  return(0);
+
+}
