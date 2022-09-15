eb55a512164904239ba93c32786993848482b8d6
Gbenga <slamyg4u@ymail.com>
2022-09-15 14:02:21 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * main - Prints Holberton
- * @void: Empty parameter list for main.
- *
- * Description: Prints Holberton w/out stdio
- *
- * Return: 0 for success
+ * main - Prints putchar
+ * 
+ * Return: Always 0
  */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
 _putchar('t');
-_putchar('o');
-_putchar('n');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
