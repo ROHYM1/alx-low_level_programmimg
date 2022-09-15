47865d7e16e79e233fca3df8bfdaf8ed80541353
Gbenga <slamyg4u@ymail.com>
2022-09-15 16:52:55 -0700

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
 /**
- * _isalpha - function to check sign of number
+ * _abs - function that computer absolute interger value
  * @c: is the int that will use for the argument of the function
  * Return: 0
  */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	  {
+	    return (c);
+	  }
+	else
+	  {
+	    return (c * -1);
+	  }
+}
