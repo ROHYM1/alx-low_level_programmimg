7d4a3a10262f8232ceefb2bba4252aca4db7bec1
Gbenga <slamyg4u@ymail.com>
2022-09-15 18:40:07 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,46 +6,26 @@
  * Return: 0
  */
 void jack_bauer(void)
-  
 {
-  int hours = 0;
-  
-  int minutes = 0;
-  
-  int hours_remainder;
-  
-  int mins_remainder;
-  
- while (hours <= 23)
-    
-    {
-      
-      while (minutes <= 59)
-	
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+	while (hours <= 23)
 	{
-	  
-	  hours_remainder = hours % 10;
-	  
-	  _putchar(hours / 10 + '0');
-	  
-	  _putchar(hours_remainder + '0');
-	  
-	  _putchar(':');
-	  
-	  _putchar(minutes / 10 + '0');
-	  
-	  _putcha(mins_remainder + '0');
-	  
-	  minutes++;
-	  
-	  _putchar('\n')
-	    
-	    }
-      
-      hours++;
-  
-  minutes = 0;
- 
-    }
+	while (minutes <= 59)
+	{
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0');
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_remainder + '0'
+	minutes++;
+	_putchar('\n')
+	}
+	hours++;
+	minutes = 0;
+	}
   
 }
