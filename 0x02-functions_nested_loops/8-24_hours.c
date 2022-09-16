1023c55eb18043050c73b8902fd2ae157ba534e8
Gbenga <slamyg4u@ymail.com>
2022-09-15 18:17:00 -0700

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * jaack_bauer - function that prints every minutes of jack
+ * n from 00:00 to 23:59, mn loop counts mins
+ * Return: 0
+ */
+void jack_bauer(void)
+  
+{
+  int hours = 0;
+  
+  int minutes = 0;
+  
+  int hours_reminder;
+  
+  int mins_rminder;
+  
+ while (hours <= 23)
+    
+    {
+      
+      while (minutes <= 59)
+	
+	{
+	  
+	  hours_remider = hours % 10;
+	  
+	  _putchar(hours / 10 + '0');
+	  
+	  _putchar(hours_reminder + '0');
+	  
+	  _putchar(':');
+	  
+	  _putchar(minutes / 10 + '0');
+	  
+	  _putcha(mins_reminder + '0');
+	  
+	  minutes++;
+	  
+	  _putchar('\n')
+	    
+	    }
+      
+hours++
+  
+  minutes = 0;
+ 
+    }
+  
+}
