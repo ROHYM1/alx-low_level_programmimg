84417cbdb8ca80e3e8a6fe8f461ce16c52f1e9d8
Gbenga <slamyg4u@ymail.com>
2022-09-19 11:52:55 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts prints a string
+ * @str: string to print
+ * Return: nothing
+ */
+
+void _puts(char *str)
+
+{
+	int r = 0;
+	while (*(str + r) != '\0')
+	{
+	putchar(*(str + r));
+	r++;
+	}
+	putchar(10);
+
+}
