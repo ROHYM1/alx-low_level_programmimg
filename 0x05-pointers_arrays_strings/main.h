893e6ea404866770e764cf17690eda92c95ba64e
Gbenga <slamyg4u@ymail.com>
2022-09-19 06:50:12 -0700

    head

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ * char prototypes (char)
+ */
+
+void reset_to_98(int);
+void swap_int(int, int);
+void _puts(char);
+int _strlen(char);
+void print_rev(char);
+void rev_string(char);
+void puts2(char);
+void puts_half(char);
+void print_array(int, int);
+char *_strcpy(char, char);
+
+
+#endif /* MAIN_H */
