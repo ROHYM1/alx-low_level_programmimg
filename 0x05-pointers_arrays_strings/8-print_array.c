207e9bcd53fa0f81f7ebb526e15d9ae079581bff
Gbenga <slamyg4u@ymail.com>
2022-09-20 00:59:50 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,8 @@ void print_array(int *a, int n)
 	int c;
 	for (c = 0; c < n; c++)
 	{
-	printf("%d", a[i]);
-	if (i != n - 1)
+	printf("%d", a[c]);
+	if (c != n - 1)
 	printf(", ");
 	}
 	putchar(10);
