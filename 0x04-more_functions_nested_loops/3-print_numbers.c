8559384b8c287df7fa9988c2fae9654d2561fa5d
Gbenga <slamyg4u@ymail.com>
2022-09-22 09:35:09 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,15 @@
 
 /**
  * print numbers - function that print all numbers from 0 to 9
- * 
+ *
  * Return: returns nothing
  */
 void print_numbers(void)
 {
 
-int n;
+char n;
 
-for (n = 48; n <= 58; n++)
+for (n = 48; n < 58; n++)
 
 {
 
