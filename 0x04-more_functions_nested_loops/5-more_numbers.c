4f6f0125afd6a4cac09fd5491975e28dbfbf4120
Gbenga <slamyg4u@ymail.com>
2022-09-22 10:19:54 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 
 int n1, n2;
 
-for (n2 = 0; n2 <= 10; n2++)
+for (n2 = 0; n2 <= 9; n2++)
 
 {
 
@@ -19,15 +19,15 @@ for (n1 = 0; n1 <= 14; n1++)
 
 {
 
-if (n2 > 9)
+if (n1 > 9)
 
 {
 
-putchar((n2 / 10) + '0');
+putchar((n1 / 10) + '0');
 
 }
 
-putchar((n2 % 10) + '0');
+putchar((n1 % 10) + '0');
 
 }
 putchar('\n');
