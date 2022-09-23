a1fcde8887245794a6ce7c6b3168e415f0692ebb
Gbenga <slamyg4u@ymail.com>
2022-09-23 12:03:10 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,22 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
+
 int largtest;
 
-if(a > b && a > c)
-
+if(a >= b && a >= c)
 {
 largest = a;
 }
-else if (b > c)
 
+else if (b >= a && b >= c)
 {
 largest = b;
 }
-else
 
+else
 {
 largest = c;
 }
+
 return (largest);
 }
