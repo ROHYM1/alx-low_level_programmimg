bec19957649b5a17cbe43938f774a05b5b26107f
Gbenga <slamyg4u@ymail.com>
2022-09-23 16:01:30 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,12 +16,12 @@ if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 
 if (month > 2 && day >= 60)
 {
-day++
+day++;
 }
 
-printf("Day of the year: %d\n", day};
+printf("Day of the year: %d\n", day);
 
-printf("Remaining days: %d\n". 366 - day);
+printf("Remaining days: %d\n", 366 - day);
 
 }
 else
