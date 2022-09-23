afc60afcd5c89d74f334505776c8ef3ce2769262
Gbenga <slamyg4u@ymail.com>
2022-09-23 10:55:45 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,12 +2,13 @@
 #include "main.h"
 
 /**
- * main - ptints positive or negative integer
- * Return: 0
+ * main - prints positive or negative integer
+ * Return: Always success
  */
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
 
