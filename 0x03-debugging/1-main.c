c6a12f7b709db1cca865c957f5520b115171c346
Gbenga <slamyg4u@ymail.com>
2022-09-23 14:55:47 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,7 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
+ 
 i = 0;
 
 /*
@@ -19,7 +20,7 @@ i = 0;
  * }
  */
 
-printf("Infinite loop avoided! \\0/\n");
+printf("Infinite loop avoided!\\0/\n");
 
 return (0);
 
