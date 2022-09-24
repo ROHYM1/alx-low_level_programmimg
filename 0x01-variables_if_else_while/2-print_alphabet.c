d3ee1f956ac74cb092b6c4371e6cbea517c8dc58
Gbenga <slamyg4u@ymail.com>
2022-09-24 16:06:33 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - determines if the value of the random number
+ * * generated is positive, zero or negative
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
