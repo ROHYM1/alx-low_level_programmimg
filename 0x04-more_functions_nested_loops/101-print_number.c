f453e3ec999c8e8f925666033947cee941efad12
Gbenga <slamyg4u@ymail.com>
2022-09-24 17:48:25 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,136 +1,25 @@
-/**
-
- * exponent - x to the power of y
-
- * @x: base number
-
- * @y: exponent
-
- * Description: calcuates x^y
-
- * Return: x^y
-
- *
-
- **/
-
-
-
-int exponent(int x, int y)
-  
-{
-  
-  int power;
-  
-
-  
-  power = x;
-  
-
-  
-  if (x == 0)
-    
-    return (0);
-  
-  if (y == 0)
-    
-    return (1);
-  
-
-  
-  while (y >= 2)
-    
-    {
-      
-      power  = power * x;
-      
-      y--;
-      
-    }
-  
-  return (power);
-  
-}
-
-
+#include "main.h"
 
 /**
-
- * print_number - print an int using only _putchar
-
- * @number: int to be printed by function
-
- *
-
- * Return: nothing
-
- **/
-
-
-
-void print_number(int number)
-  
+ * print_number - Prints an integer.
+ * @n: integer to be printed.
+ */
+void print_number(int n)
 {
-  
-  int size, digit;
-  
-  long counter, sign;
-  
 
-  
-  sign = 1;
-  
-  digit = 0;
-  
-  size = 1;
-  
-  counter = number;
-  
+unsigned int num = n;
 
-  
-  if (number < 0)
-    
-    {
-      
-      _putchar('-');
-      
-      sign = -1;
-      
-      counter *= sign;
-      
-    }
-  
+if (n < 0)
 
-  
-  for (; counter >= 10; size++)
-    
-    {
-      
-      counter = counter / 10;
-      
-    }
-  
+{
+putchar('-');
 
-  
-  counter = sign * (long)number;
-  
-
-  
-  while (size >= 2)
-    
-    {
-      
-      digit = (counter / exponent(10, size - 1));
-      
-      _putchar(digit + '0');
-      
-      counter = counter % exponent(10, size - 1);
-      
-      size--;
-      
-    }
-  
-  _putchar(counter % 10 + '0');
-  
+num = -num;
 }
 
+if ((num / 10) > 0)
+
+print_number(num / 10);
+
+putchar((num % 10) + '0');
+}
