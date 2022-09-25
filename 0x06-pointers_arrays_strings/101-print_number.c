4632ab1f7f385b2c3d70a1c271d32aeb13810142
Gbenga <slamyg4u@ymail.com>
2022-09-25 07:47:29 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 int m, d, count;
 if (n < 0)
 {
-_putchar(45);
+putchar(45);
 m = n * -1;
 }
 else
@@ -27,6 +27,6 @@ count *= 10;
 }
 for (; count >= 1; count /= 10)
 {
-_putchar(((m / count) % 10) + 48);
+putchar(((m / count) % 10) + 48);
 }
 }
